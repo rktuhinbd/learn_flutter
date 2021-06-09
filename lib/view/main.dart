b99da525079d6047b80a840af49ba5d036672377
Md. Rejaul Karim <rktuhinbd@gmail.com>
2021-06-09 15:52:10 +0600

    Sign up textfield code optimized and updated, added label text in textfield, removed text heading for input fields

--- lib/view/main.dart
+++ lib/view/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'Home.dart';
+import 'ListExample.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false, home: Home());
   }
 }
